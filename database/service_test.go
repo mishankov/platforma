01d3f59117eb82ff7597a30c36d6a2de6f117e66
Denis Mishankov <mishankov@mail.com>
2025-10-27 20:33:12 +0300

    error test for SaveMigrationLogs

--- database/service_test.go
+++ database/service_test.go
@@ -2,6 +2,7 @@ package database_test
 
 import (
 	"context"
+	"errors"
 	"testing"
 
 	"github.com/jmoiron/sqlx"
@@ -9,7 +10,9 @@ import (
 )
 
 func TestSaveMigrationLogs(t *testing.T) {
+	t.Parallel()
 	t.Run("single successful", func(t *testing.T) {
+		t.Parallel()
 		repo := &repoMock{}
 		service := database.NewService(repo, &sqlx.DB{})
 
@@ -18,15 +21,70 @@ func TestSaveMigrationLogs(t *testing.T) {
 			t.Fatalf("expected no errors, got: %s", err.Error())
 		}
 	})
+
+	t.Run("single error", func(t *testing.T) {
+		t.Parallel()
+		ErrSome := errors.New("some error")
+		repo := &repoMock{saveMigrationLog: func(ctx context.Context, ml database.MigrationLog) error {
+			return ErrSome
+		}}
+		service := database.NewService(repo, &sqlx.DB{})
+
+		err := service.SaveMigrationLogs(context.TODO(), []database.Migration{{ID: "some id"}})
+		if err == nil {
+			t.Fatalf("expected error, got nothing")
+		}
+
+		if !errors.Is(err, ErrSome) {
+			t.Fatalf("expected ErrSome, got: %s", err.Error())
+		}
+	})
+
+	t.Run("multiple errors", func(t *testing.T) {
+		t.Parallel()
+		ErrSome := errors.New("some error")
+		ErrOther := errors.New("other error")
+		repo := &repoMock{saveMigrationLog: func(ctx context.Context, ml database.MigrationLog) error {
+			if ml.MigrationId == "some id" {
+				return ErrSome
+			}
+
+			if ml.MigrationId == "other id" {
+				return ErrOther
+			}
+
+			return nil
+		}}
+		service := database.NewService(repo, &sqlx.DB{})
+
+		err := service.SaveMigrationLogs(context.TODO(), []database.Migration{{ID: "some id"}, {ID: "other id"}})
+		if err == nil {
+			t.Fatalf("expected error, got nothing")
+		}
+
+		if !errors.Is(err, ErrSome) {
+			t.Fatalf("expected ErrSome, got: %s", err.Error())
+		}
+
+		if !errors.Is(err, ErrOther) {
+			t.Fatalf("expected ErrOther, got: %s", err.Error())
+		}
+	})
 }
 
-type repoMock struct{}
+type repoMock struct {
+	saveMigrationLog func(context.Context, database.MigrationLog) error
+}
 
 func (r *repoMock) GetMigrationLogs(ctx context.Context) ([]database.MigrationLog, error) {
 	return nil, nil
 }
 
 func (r *repoMock) SaveMigrationLog(ctx context.Context, log database.MigrationLog) error {
+	if r.saveMigrationLog != nil {
+		return r.saveMigrationLog(ctx, log)
+	}
+
 	return nil
 }
 
