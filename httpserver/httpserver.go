3546334c2e1aab48e0ceba4b633184bf83ba0102
Denis Mishankov <mishankov@mail.com>
2025-10-29 16:13:40 +0300

    Refactor HttpServer to use embedded HandlerGroup

--- httpserver/httpserver.go
+++ httpserver/httpserver.go
@@ -14,40 +14,13 @@ import (
 )
 
 type HttpServer struct {
-	mux             *http.ServeMux
+	HandlerGroup
 	port            string
 	shutdownTimeout time.Duration
-	middlewares     []Middleware
 }
 
 func New(port string, shutdownTimeout time.Duration) *HttpServer {
-	return &HttpServer{mux: http.NewServeMux(), port: port, shutdownTimeout: shutdownTimeout}
-}
-
-func (s *HttpServer) Handle(pattern string, handler http.Handler) {
-	s.mux.Handle(pattern, handler)
-}
-
-func (s *HttpServer) HandleFunc(pattern string, handler func(http.ResponseWriter, *http.Request)) {
-	s.mux.HandleFunc(pattern, http.HandlerFunc(handler))
-}
-
-func (s *HttpServer) HandleGroup(pattern string, handler http.Handler) {
-	s.mux.Handle(pattern+"/", http.StripPrefix(pattern, handler))
-}
-
-func (s *HttpServer) Use(middlewares ...Middleware) {
-	s.middlewares = append(s.middlewares, middlewares...)
-}
-
-func (s *HttpServer) UseFunc(middlewareFuncs ...func(http.Handler) http.Handler) {
-	for _, middlewareFunc := range middlewareFuncs {
-		s.middlewares = append(s.middlewares, MiddlewareFunc(middlewareFunc))
-	}
-}
-
-func (s *HttpServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	wrapHandlerInMiddleware(s.mux, s.middlewares).ServeHTTP(w, r)
+	return &HttpServer{HandlerGroup: HandlerGroup{mux: http.NewServeMux()}, port: port, shutdownTimeout: shutdownTimeout}
 }
 
 func (s *HttpServer) Run(ctx context.Context) error {
