7ab968e603fbce29a85a1ce451c217642c4debed
Denis Mishankov <mishankov@mail.com>
2025-11-07 16:08:17 +0300

    Move fileserver to httpserver package and simplify implementation

---
+++ httpserver/fileserver.go
@@ -0,0 +1,31 @@
+package httpserver
+
+import (
+	"context"
+	"io/fs"
+	"net/http"
+	"time"
+)
+
+// FileServer represents an HTTP file server for serving static files.
+type FileServer struct {
+	server *HTTPServer
+	port   string
+}
+
+// New creates a new FileServer instance with the given file system, base path, and port.
+func NewFileServer(fs fs.FS, basePath, port string) *FileServer {
+	server := New(port, 1*time.Second)
+	server.HandleGroup(basePath, http.FileServer(http.FS(fs)))
+
+	return &FileServer{server: server}
+}
+
+func (s *FileServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	s.server.ServeHTTP(w, r)
+}
+
+// Run starts the file server and listens for incoming requests.
+func (s *FileServer) Run(ctx context.Context) error {
+	return s.server.Run(ctx)
+}
--- internal/cli/docs.go
+++ internal/cli/docs.go
@@ -4,13 +4,13 @@ import (
 	"context"
 
 	"github.com/mishankov/platforma/docs"
-	"github.com/mishankov/platforma/fileserver"
+	"github.com/mishankov/platforma/httpserver"
 	"github.com/mishankov/platforma/log"
 )
 
 func docsCommand(_ []string) {
 	ctx := context.Background()
-	server := fileserver.New(docs.Assets(), "/platforma/", "4444")
+	server := httpserver.NewFileServer(docs.Assets(), "/platforma", "4444")
 
 	if err := server.Run(ctx); err != nil {
 		log.ErrorContext(ctx, "documentation serving ended with error", "error", err)
