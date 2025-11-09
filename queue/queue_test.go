c488e9eb4a2f0b5b1dc7bf31244a4bdbd1cbf005
Denis Mishankov <mishankov@mail.com>
2025-11-09 12:52:52 +0300

    basic test for new logic

--- queue/queue_test.go
+++ queue/queue_test.go
@@ -2,7 +2,6 @@ package queue_test
 
 import (
 	"context"
-	"sync"
 	"testing"
 	"time"
 
@@ -12,23 +11,24 @@ import (
 func TestQueue(t *testing.T) {
 	t.Parallel()
 
-	type job struct {
-		data int
-	}
-
 	t.Run("simple queue", func(t *testing.T) {
 		t.Parallel()
+		ctx := context.Background()
 		res := 0
 
-		q := queue.New(queue.HandlerFunc[job](func(ctx context.Context, job job) {
+		q := &mockQueue[job]{
+			jobChan: make(chan job, 10),
+		}
+
+		p := queue.New(queue.HandlerFunc[job](func(ctx context.Context, job job) {
 			res += job.data
-		}), 3, 4, time.Microsecond)
+		}), q, 4, time.Microsecond)
 
-		go q.Run(context.TODO())
+		go p.Run(context.TODO())
 
-		q.Enqueue(job{data: 1})
-		q.Enqueue(job{data: 1})
-		q.Enqueue(job{data: 1})
+		p.Enqueue(ctx, job{data: 1})
+		p.Enqueue(ctx, job{data: 1})
+		p.Enqueue(ctx, job{data: 1})
 
 		time.Sleep(1 * time.Millisecond)
 
@@ -36,38 +36,29 @@ func TestQueue(t *testing.T) {
 			t.Errorf("expected res to be 1, got %d", res)
 		}
 	})
+}
 
-	t.Run("shutdown", func(t *testing.T) {
-		t.Parallel()
-		res := 0
-		ctx, cancel := context.WithCancel(context.Background())
+type job struct {
+	data int
+}
 
-		// Defining queue with slow running handler
-		q := queue.New(queue.HandlerFunc[job](func(ctx context.Context, job job) {
-			time.Sleep(3 * time.Second)
-			res += job.data
-		}), 1, 10, 1*time.Second)
+type mockQueue[T any] struct {
+	jobChan chan T
+}
 
-		// Start queue
-		var wg sync.WaitGroup
-		wg.Go(func() {
-			q.Run(ctx)
-		})
+func (q *mockQueue[T]) Open(ctx context.Context) error {
+	return nil
+}
 
-		// Enqueue 5 jobs
-		for range 5 {
-			q.Enqueue(job{data: 1})
-		}
+func (q *mockQueue[T]) Close(ctx context.Context) error {
+	return nil
+}
 
-		// Wait a bit for workers to pickup jobs
-		time.Sleep(1 * time.Second)
+func (q *mockQueue[T]) EnqueueJob(ctx context.Context, job T) error {
+	q.jobChan <- job
+	return nil
+}
 
-		// Cancelling context and waiting for queue to stop
-		cancel()
-		wg.Wait()
-
-		if res != 2 {
-			t.Errorf("expected res to be 2, got %d", res)
-		}
-	})
+func (q *mockQueue[T]) GetJobChan(ctx context.Context) (chan T, error) {
+	return q.jobChan, nil
 }
