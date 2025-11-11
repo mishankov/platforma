ead2dc835c6f6317d5a4e873d0adc12d690dcebc
Denis Mishankov <mishankov@mail.com>
2025-11-11 12:17:43 +0300

    Add channel-based queue implementation tests

---
+++ queue/chanqueue_test.go
@@ -0,0 +1,42 @@
+package queue_test
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	"github.com/platforma-dev/platforma/queue"
+)
+
+func TestChanQueue(t *testing.T) {
+	t.Run("simple enqueue", func(t *testing.T) {
+		ctx := context.Background()
+		q := queue.NewChanQueue[job](3, time.Second)
+
+		err := q.Open(ctx)
+		if err != nil {
+			t.Fatalf("expected no error, got: %s", err.Error())
+		}
+		defer q.Close(ctx)
+
+		err = q.EnqueueJob(ctx, job{data: 1})
+		if err != nil {
+			t.Fatalf("expected no error, got: %s", err.Error())
+		}
+
+		ch, err := q.GetJobChan(ctx)
+		if err != nil {
+			t.Fatalf("expected no error, got: %s", err.Error())
+		}
+
+		select {
+		case j := <-ch:
+			if j.data != 1 {
+				t.Fatalf("expected data to be 1, got: %d", j.data)
+			}
+		default:
+			t.Fatalf("expected job to be received from channel")
+		}
+	})
+
+}
